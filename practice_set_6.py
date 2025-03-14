94971097124890f7d91fa071eb59cf3bf8245fb2
Piyush Verma <piyush.vermapv38@gmail.com>
2025-03-14 10:30:48 +0530

    Update practice_set_6.py

--- practice_set_6.py
+++ practice_set_6.py
@@ -92,8 +92,8 @@ else:
 '''Q7> Write a program to find out whether a given post is talking about "python" or not.'''
 
 post = input("Enter the post: ")
-if("Python" in post):
-    print("Yes the post is taling about Python")
+
+if("Python".lower() in post.lower()):
+    print("Yes the post is talking about python")
 else:
-    print("This post is not talking about Python")
-print("Thank you")
+    print("The post is not talking about pyhton")
